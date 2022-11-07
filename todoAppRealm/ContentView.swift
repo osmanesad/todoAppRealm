6b5881fac1ade4f963dca80ab76a9179b461ee87
osmanesad <byesad@gmail.com>
2022-11-07 23:17:53 +0300

    Initial Commit

---
+++ todoAppRealm/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  todoAppRealm
+//
+//  Created by Osman Esad on 7.11.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ todoAppRealm/todoAppRealmApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoAppRealmApp.swift
+//  todoAppRealm
+//
+//  Created by Osman Esad on 7.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct todoAppRealmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
