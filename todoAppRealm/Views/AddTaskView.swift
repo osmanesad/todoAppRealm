004eb26b69d1d1c334ae68db3da80dd3ceef2bdd
osmanesad <byesad@gmail.com>
2022-11-09 18:49:39 +0300

    Update - AddTaskView
    Görev listesine yeni görev ekelemek için alan eklendi.

--- todoAppRealm/Views/AddTaskView.swift
+++ todoAppRealm/Views/AddTaskView.swift
@@ -8,10 +8,27 @@
 import SwiftUI
 
 struct AddTaskView: View {
+    
+    @State private var title: String = ""
+    
     var body: some View {
         VStack(alignment: .leading, spacing: 20){
             Text("Yeni görev oluştur.")
                 .font(.title3).bold()
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            TextField("Yeni görevi ekle", text: $title)
+                .textFieldStyle(.roundedBorder)
+            
+            Button{
+                print("Yeni görev eklendi.")
+            } label: {
+                Text("Ekle")
+                    .foregroundColor(.white)
+                    .padding()
+                    .padding(.horizontal)
+                    .background(.green)
+            }
         }
         .padding(.top, 40)
         .padding(.horizontal)
